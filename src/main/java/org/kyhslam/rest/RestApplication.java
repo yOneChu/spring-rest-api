eb77c04efaa62ff0848fb2196835b8ee3688ecbd
kyhslam <kyhslam@gmail.com>
2020-07-27 00:08:38 +0900

    DTO, ObjectMapper Test

--- src/main/java/org/kyhslam/rest/RestApplication.java
+++ src/main/java/org/kyhslam/rest/RestApplication.java
@@ -1,12 +1,19 @@
 package org.kyhslam.rest;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 }
